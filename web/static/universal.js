34167da7871be1cb1386aa3d5417d9515fc3b50c
Loris Vonlanthen <vonlanthen.loris@gmail.com>
2023-05-20 16:02:01 +0200

    Map with markers and  selects

--- web/static/universal.js
+++ web/static/universal.js
@@ -1,4 +1,11 @@
 var map = L.map('map').fitWorld();
+map.setView(new L.LatLng(47.751569, 1.675063), 4);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+function changeUrl() {
+    category = document.getElementById("category").value;
+    year = document.getElementById("year").value;
+    window.location.href = "/data?category=" + category + "&year=" + year;
+}
